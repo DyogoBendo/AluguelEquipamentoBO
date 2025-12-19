2fd12e8810b991e4d8406e514fea0be8ec92a087
dyogo.bendo <dyogoromagnabendo@gmail.com>
2025-12-19 09:45:39 -0300

    update

--- src/main/java/unioeste/geral/aluguel/bo/Cliente.java
+++ src/main/java/unioeste/geral/aluguel/bo/Cliente.java
@@ -1,7 +1,15 @@
 package unioeste.geral.aluguel.bo;
+import unioeste.geral.endereco.bo.EnderecoEspecifico;
 import unioeste.geral.pessoa.bo.*;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Cliente extends PessoaFisica implements Serializable {
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String nomeSocial, EnderecoEspecifico enderecoResidencial, List<Fone> fones, List<Email> emails, String cpf, String primeiroNome, String sobreNome) {
+        super(id, nome, nomeSocial, enderecoResidencial, fones, emails, cpf, primeiroNome, sobreNome);
+    }
 }
